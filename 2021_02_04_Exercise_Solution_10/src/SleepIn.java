b8edc8ddca29270562541e5cfe33dee61b3d635d
Vitalii Vagushchenko <vvagushchenko@gmail.com>
2021-02-16 18:15:58 +0100

    это сделано

--- 2021_02_04_Exercise_Solution_10/src/SleepIn.java
+++ 2021_02_04_Exercise_Solution_10/src/SleepIn.java
@@ -1,16 +1,14 @@
 public class SleepIn {
     public static void main(String[] args) {
         /*The parameter weekday is true if it is a weekday,
-         and the parameter vacation is true if we are on vacation.
-         We sleep in if it is not a weekday or we’re on vacation. Return true if we sleep in.
+        and the parameter vacation is true if we are on vacation.
+        We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
          */
-
-        System.out.println(sleepIn(false, false)); // → true
-        System.out.println(sleepIn(true, false)); //→ false
-        System.out.println(sleepIn(false, true)); //→ true
-        System.out.println(sleepIn(true, true)); //→ true
+        System.out.println(sleepIn(false, false));//true
+        System.out.println(sleepIn(true, false));// → false
+        System.out.println(sleepIn(false, true));// → true
+        System.out.println(sleepIn(true, true));// → true
     }
-
     private static boolean sleepIn(boolean weekday, boolean vacation) {
         if (weekday != true || vacation == true){
             return true;
