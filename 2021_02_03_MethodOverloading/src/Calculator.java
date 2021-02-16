0c3da94734dd903cbc474811f47aa6208d614539
Vitalii Vagushchenko <vvagushchenko@gmail.com>
2021-02-16 18:12:50 +0100

    урок 1

--- 2021_02_03_MethodOverloading/src/Calculator.java
+++ 2021_02_03_MethodOverloading/src/Calculator.java
@@ -1,21 +1,37 @@
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println(addition( 10, 5.0));
+        System.out.println(addition(10, 5.0));
     }
 
-    public static int Addition (int a, int b) {
-        return a + b;
-    }
-
-    public static double doubleSum (double a, double b) {
-        return a + b;
-    }
-    public static String addition (String a, String b){
-        return a + b;
-    }
-    public static int addition (int a, double b){
+    public static int addition() {
         return 0;
     }
 
-}
+    public static int addition(int[] input) {
+        return 0;
+    }
 
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+
+    public static double addition(double a, double b) {
+        return a + b;
+    }
+
+    public static String addition(String a, String b) {
+        return a + b;
+    }
+
+    public static int addition(int a, double b) {
+        return 0;
+    }
+
+    public static void addition(double a, int b) {
+
+    }
+
+    public static double addition(double a, double b, double c) {
+        return 0.0;
+    }
+}
