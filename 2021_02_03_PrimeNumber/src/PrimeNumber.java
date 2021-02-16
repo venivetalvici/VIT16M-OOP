ddf799bed44b1bdea051095efacd0e62f2f0e343
Vitalii Vagushchenko <vvagushchenko@gmail.com>
2021-02-16 18:14:45 +0100

    урок 1

--- 2021_02_03_PrimeNumber/src/PrimeNumber.java
+++ 2021_02_03_PrimeNumber/src/PrimeNumber.java
@@ -1,28 +1,30 @@
+
 public class PrimeNumber {
     public static void main(String[] args) {
-        // дано число найти все числа меньше данного числа.
-        findAllPrimeNumber (100);
+        // Дано число найти все ПРОСТЫЕ ЧИСЛА меньше данного числа.
+        findAllPrimeNumber(1000_000);
+        System.out.println(isPrimeNumber(10001));
 
     }
 
     private static void findAllPrimeNumber(int n) {
         for (int i = 2; i < n; i++) {
-         if (isPrimeNumber(i)) {
-             System.out.print(i + ", ");
-         }
+            if (isPrimeNumber(i)) {
+                System.out.print(i + ", ");
+            }
         }
     }
 
+    // n sqrt(n)
+    // n в степени н
+    //
     private static boolean isPrimeNumber(int input) {
         int border = (int) Math.sqrt(input);
-        for (int i = 2; i <= border ; i++) {
+        for (int i = 2; i <= border; i++) {
             if (input % i == 0) {
                 return false;
             }
         }
         return true;
     }
-
 }
-
-
